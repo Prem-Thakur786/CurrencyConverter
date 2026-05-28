d4f9412ba5b34e79dacb7469e0ebd8a930e90bd9
Prem Thakur <pkpremchand786@gmail.com>
2026-05-28 20:31:41 +0500

    updated componet for footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react'
 import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -68,19 +69,7 @@ function App() {
             </div>
         </div>
 
-        <footer className="w-full py-3 text-center backdrop-blur-sm bg-black/30">
-            <p className="text-white/60 text-xs tracking-wide">
-                Made with <span className="text-red-400">❤️</span> by{" "}
-                <a
-                    href="https://github.com/Prem-Thakur786"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-blue-300 font-semibold hover:text-white transition-colors duration-200"
-                >
-                    Prem.Thakur1618
-                </a>
-            </p>
-        </footer>
+      <Footer />
     </div>
 );
 }
